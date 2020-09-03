052f7525f988b37af85dcf30a0c1ce2be88ba527
Tanner Phillips <tphillips@users.noreply.github.com>
2020-09-03 15:10:21 -0500

    Update script.js
    Added in array of quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,12 +11,43 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: "Life is like a box of chocolates. You never know what you're gonna get.",
+    author: "Tom Hanks",
+    date: "1994",
+    origin: "Forrest Gump"
+  },
+  {
+    quote: "Seize the day, boys. Make your lives extraordinary.",
+    author: "Robin Williams",
+    date: "1989",
+    origin: "Dead Poets Society"
+  },
+  {
+    quote: "Keep your friends close, but your enemies closer.",
+    author: "Al Pacino",
+    date: "1974",
+    origin: "The Godfather part II"
+  },
+  {
+    quote: "With great power comes great responsibility.",
+    author: "Cliff Robertson",
+    date: "2002",
+    origin: "Spider-Man"
+  },
+  {
+    quote: "I don't have friends. I got family",
+    author: "Vin Diesel",
+    date: "2015",
+    origin: "Furious 7"
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
 
 
 /***
